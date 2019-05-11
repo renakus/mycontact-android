e8d80281573cae18f91831671c6f556ff84344dc
renakus <rkus231@gmail.com>
2019-05-11 13:36:56 +0700

    first commit

---
+++ app/src/main/java/net/renakus/mycontact/ContactFragment.kt
@@ -0,0 +1,29 @@
+package net.renakus.mycontact
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
+
+class ContactFragment(): Fragment{
+    companion object {
+        fun newInstance(): ContactFragment{
+            return ContactFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.contact_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+}
